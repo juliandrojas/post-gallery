67a9520f92723ff758945dbfa1a77c2bf281cb20
Julián Rojas <juliandra140201@gmail.com>
2023-06-22 14:32:56 -0500

    Update of the title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 export default function Title() {
   return (
-      <h1 className="text-center">Galería de posts con React</h1>
+      <h1 className="text-center">Galería de posts de una API con React</h1>
   );
 }
